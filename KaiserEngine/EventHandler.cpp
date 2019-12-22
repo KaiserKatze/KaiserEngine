2e34b024d151539c6292c50eb539c5416d4ce365
KaiserKatze <donizyo@gmail.com>
2019-12-22 15:14:49 +0800

    Update EventHandler.cpp

--- KaiserEngine/EventHandler.cpp
+++ KaiserEngine/EventHandler.cpp
@@ -1,3 +1,29 @@
 #include "stdafx.h"
 #include "EventHandler.h"
 
+LRESULT
+EventHandler::
+TraverseList(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    for (auto itr = listeners.begin();
+        itr != listeners.end();
+        itr++)
+    {
+        itr->get()->Handle(message, wParam, lParam);
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+void
+EventHandler::
+AddListener(EventListener* listener)
+{
+    listeners.push_back(SPEventListener(listener));
+}
+
+void
+EventHandler::
+RemoveAllListeners()
+{
+    listeners.clear();
+}
