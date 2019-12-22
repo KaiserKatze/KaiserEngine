b9dcbf8e5a7c7460ecfb525d5d197dc3f814f1cd
KaiserKatze <donizyo@gmail.com>
2019-12-22 14:57:01 +0800

    Update Event.h

--- KaiserEngine/Event.h
+++ KaiserEngine/Event.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include "stdafx.h"
+
+class MouseEvent
+{
+public:
+    MouseEvent(UINT message, WPARAM wParam, LPARAM lParam);
+    int getButton() const;
+    POINT getPoint() const;
+    bool isCtrlKeyDown() const;
+    bool isShiftKeyDown() const;
+private:
+    const int button;
+    const int combination;
+    const POINT point;
+};
