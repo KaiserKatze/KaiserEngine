06c388941f1bc6fca8a34e3f516fbc8da4a380bb
KaiserKatze <donizyo@gmail.com>
2019-12-23 00:23:22 +0800

    Update EventManager.cpp

--- KaiserEngine/EventManager.cpp
+++ KaiserEngine/EventManager.cpp
@@ -2,15 +2,21 @@
 #include "EventManager.h"
 
 EventManager::
-EventManager(const HWND& hWnd)
+EventManager()
 {
-    this->AddListener(new WindowEventListener(hWnd));
-    this->AddListener(new KeyboardEventListener(hWnd));
-    this->AddListener(new MouseEventListener(hWnd));
 }
 
 EventManager::
 ~EventManager()
 {
     this->RemoveAllListeners();
+}
+
+void
+EventManager::
+Setup(const HWND& hWnd)
+{
+    AddListener(new WindowEventListener(hWnd));
+    AddListener(new KeyboardEventListener(hWnd));
+    AddListener(new MouseEventListener(hWnd));
 }
