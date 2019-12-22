514a553dcb938c55ad8b186d6c7005fa7a9bbd08
KaiserKatze <donizyo@gmail.com>
2019-12-23 00:28:46 +0800

    Update EventListener.h

--- KaiserEngine/EventListener.h
+++ KaiserEngine/EventListener.h
@@ -2,3 +2,47 @@
 
 #include "stdafx.h"
 #include "Event.h"
+
+class EventListener abstract
+{
+public:
+    EventListener(const HWND& hWnd);
+    EventListener(const EventListener&) = delete;
+    ~EventListener();
+    virtual LRESULT Handle(UINT message, WPARAM wParam, LPARAM lParam) const = 0;
+protected:
+    const HWND& getWindowHandle() const;
+private:
+    const HWND& hWnd;
+};
+
+class KeyboardEventListener : public EventListener
+{
+public:
+    KeyboardEventListener(const HWND& hWnd);
+    LRESULT Handle(UINT message, WPARAM wParam, LPARAM lParam) const;
+};
+
+class MouseEventListener : public EventListener
+{
+public:
+    MouseEventListener(const HWND& hWnd);
+    LRESULT Handle(UINT message, WPARAM wParam, LPARAM lParam) const;
+protected:
+    LRESULT OnClick(const MouseEvent) const;
+};
+
+class WindowEventListener : public EventListener
+{
+public:
+    WindowEventListener(const HWND& hWnd);
+    LRESULT Handle(UINT message, WPARAM wParam, LPARAM lParam) const;
+protected:
+    LRESULT OnCreate() const;
+    LRESULT OnClose() const;
+    LRESULT OnDestroy() const;
+    LRESULT OnResize(int newWdth, int newHeight) const;
+    LRESULT OnActivate(bool state) const;
+    LRESULT OnPaint() const;
+    LRESULT OnTimer() const;
+};
