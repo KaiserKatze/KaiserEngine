619f7393b6f69ec3b1c06ce280291c2c383af7c2
KaiserKatze <donizyo@gmail.com>
2019-12-27 14:35:48 +0800

    Update Window.h

--- KaiserEngine/Window.h
+++ KaiserEngine/Window.h
@@ -434,3 +434,16 @@ public:
         ::ShowWindow(getWindowHandle(), show ? SW_SHOW : SW_HIDE);
     }
 };
+
+class AttributeListFactory
+{
+public:
+    AttributeListFactory();
+    ~AttributeListFactory();
+    void put(const int& key, const int& val);
+    void clear();
+    size_t size() const;
+    const std::vector<int> make();
+private:
+    std::map<int, int> entries;
+};
