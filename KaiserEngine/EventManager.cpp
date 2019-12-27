3ee03de7aed9beb193252700a0763c9241dd04de
KaiserKatze <donizyo@gmail.com>
2019-12-27 14:49:32 +0800

    Update

--- KaiserEngine/EventManager.cpp
+++ KaiserEngine/EventManager.cpp
@@ -16,10 +16,13 @@ void
 EventManager::
 Setup(const HWND& hWnd)
 {
-    LPEventListener& listeners = GetListeners();
-    listeners.push_back(std::make_unique<WindowEventListener>(this, hWnd));
-    listeners.push_back(std::make_unique<KeyboardEventListener>(this, hWnd));
-    listeners.push_back(std::make_unique<MouseEventListener>(this, hWnd));
+    if (hWnd)
+    {
+        LPEventListener& listeners = GetListeners();
+        listeners.push_back(std::make_unique<WindowEventListener>(this, hWnd));
+        listeners.push_back(std::make_unique<KeyboardEventListener>(this, hWnd));
+        listeners.push_back(std::make_unique<MouseEventListener>(this, hWnd));
+    }
 }
 
 void
