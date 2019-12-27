dbc7d857cdab58d6e6a6054a6f804c4cc7a96d5a
KaiserKatze <donizyo@gmail.com>
2019-12-28 01:46:51 +0800

    Update Matrix.h

--- KaiserEngine/Matrix.h
+++ KaiserEngine/Matrix.h
@@ -1,2 +1,80 @@
 #pragma once
 
+template <typename _Ty, int Width, int Height>
+class Matrix
+{
+private:
+    int data[Width * Height];
+    bool isTransposed;
+
+    // pos = (row, column)
+    int convert2index(const std::pair<int, int>& pos) const
+    {
+        return convert2index(pos.first, pos.second);
+    }
+
+    int convert2index(const int& row, const int& column) const
+    {
+        if (row < 0)
+            throw std::domain_error("Invalid argument: row < 0!");
+        if (row >= Height)
+            throw std::domain_error("Invalid argument: row >= Height!");
+        if (column < 0)
+            throw std::domain_error("Invalid argument: column < 0!");
+        if (column >= Width)
+            throw std::domain_error("Invalid argument: column >= Width!");
+
+        if (isTransposed)   return row * Width + column;    // row-first
+        else                return row + column * Height;   // column-first
+    }
+
+public:
+    Matrix(const bool& isTransposed = false)
+        : isTransposed{ isTransposed }
+        , data{ 0 }
+    {
+    }
+
+    ~Matrix() {}
+
+    _Ty& operator[](const std::pair<int, int>& pos)
+    {
+        return data[convert2index(pos)];
+    }
+
+    void transpose()
+    {
+        isTransposed = !isTransposed;
+    }
+
+    // print internal data structure, regardless of whether the matrix is transposed or not
+    const std::string ToString() const
+    {
+        std::stringstream ss;
+
+        for (int row = 0; row < Height; row++)
+        {
+            ss << "| ";
+            for (int column = 0; column < Width; column++)
+            {
+                ss << data[convert2index(row, column)]
+                    << " ";
+            }
+            ss << "|"
+                << std::endl;
+        }
+
+        return ss.str();
+    }
+};
+
+// squre matrix
+template <typename _Ty, int N>
+using MatrixQ = Matrix<_Ty, N, N>;
+
+using Matrix3i = MatrixQ<int, 3>;
+using Matrix4i = MatrixQ<int, 4>;
+using Matrix3f = MatrixQ<float, 3>;
+using Matrix4f = MatrixQ<float, 4>;
+using Matrix3d = MatrixQ<double, 3>;
+using Matrix4d = MatrixQ<double, 4>;
