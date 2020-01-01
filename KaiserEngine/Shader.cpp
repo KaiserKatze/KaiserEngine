4003b177bc6b7ca76cdef68c2f5bffcfbdd3978b
KaiserKatze <donizyo@gmail.com>
2020-01-01 21:50:44 +0800

    Update Shader.cpp

--- KaiserEngine/Shader.cpp
+++ KaiserEngine/Shader.cpp
@@ -1,2 +1,21 @@
 #include "stdafx.h"
 #include "Shader.h"
+#include "loadgl.h"
+
+const std::string slurp(std::ifstream& ifs)
+{
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    return ss.str();
+}
+
+Shader::
+Shader(const std::string& path)
+    : text{ slurp(std::ifstream{path}) }
+{
+}
+
+Shader::
+~Shader()
+{
+}
