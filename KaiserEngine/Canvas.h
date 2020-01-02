d8d90097858778c6efdb1c693d14f96394eb1292
KaiserKatze <donizyo@gmail.com>
2020-01-02 21:16:10 +0800

    Update Canvas.h

--- KaiserEngine/Canvas.h
+++ KaiserEngine/Canvas.h
@@ -7,13 +7,26 @@ class Canvas
 private:
     const MainWindow& parent;
 
+    // Setup viewport
+    void setup() const;
+
 public:
     Canvas(const MainWindow& window);
     ~Canvas();
 
+    // Setup viewport
+    void setup(const int& width, const int& height) const;
+
+    // Prepare OpenGL resources
     void prepare() const;
+
+    // Clear canvas
     void clear() const;
+
+    // Render canvas
     void render() const;
+
+    // Dispose OpenGL resources
     void dispose() const;
 };
 
