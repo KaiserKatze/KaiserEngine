b0843b9ba7bb17998dd905b97d0e37296cf29b61
KaiserKatze <donizyo@gmail.com>
2020-01-15 21:51:07 +0800

    Update

--- KaiserEngine/Event.h
+++ KaiserEngine/Event.h
@@ -2,30 +2,11 @@
 
 #include "stdafx.h"
 
+class Event
+{
+};
+
 // Forward declaration
 // because "EventHandler.h" and "EventListener.h" are including each other
 class EventHandler;
 class EventListener;
-
-enum MouseButton
-{
-    UnknownButton = 0,
-    LeftButton = 1,
-    RightButton = 2,
-    MiddleButton = 3,
-};
-
-class MouseEvent
-{
-public:
-    MouseEvent(UINT message, WPARAM wParam, LPARAM lParam);
-    ~MouseEvent();
-    MouseButton getButton() const;
-    const POINT getPoint() const;
-    bool isCtrlKeyDown() const;
-    bool isShiftKeyDown() const;
-private:
-    const MouseButton button;
-    const int combination;
-    const POINT point;
-};
--- KaiserEngine/MouseEvent.h
+++ KaiserEngine/MouseEvent.h
@@ -1,2 +1,28 @@
 #pragma once
 
+#include <Windows.h>
+#include "Event.h"
+
+enum MouseButton
+{
+    UnknownButton = 0,
+    LeftButton = 1,
+    RightButton = 2,
+    MiddleButton = 3,
+};
+
+class MouseEvent
+    : public Event
+{
+public:
+    MouseEvent(UINT message, WPARAM wParam, LPARAM lParam);
+    ~MouseEvent();
+    MouseButton getButton() const;
+    const POINT getPoint() const;
+    bool isCtrlKeyDown() const;
+    bool isShiftKeyDown() const;
+private:
+    const MouseButton button;
+    const int combination;
+    const POINT point;
+};
