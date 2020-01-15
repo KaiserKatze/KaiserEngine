0b45f43b015312d60685ebfe281f4a9e74adb649
KaiserKatze <donizyo@gmail.com>
2020-01-15 22:12:31 +0800

    Update

--- KaiserEngine/KeyboardEvent.cpp
+++ KaiserEngine/KeyboardEvent.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.h"
 #include "KeyboardEvent.h"
+
+KeyboardEvent::
+KeyboardEvent(UINT message, WPARAM wParam, LPARAM lParam)
+{
+}
+
+KeyboardEvent::
+~KeyboardEvent()
+{
+}
--- KaiserEngine/KeyboardEvent.h
+++ KaiserEngine/KeyboardEvent.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <Windows.h>
+#include "Event.h"
+
+class KeyboardEvent
+    : public Event
+{
+public:
+    KeyboardEvent(UINT message, WPARAM wParam, LPARAM lParam);
+    virtual ~KeyboardEvent();
+};
