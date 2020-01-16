7ed97cc5576992bf809eabb1e0dae5d0ae9bc7db
KaiserKatze <donizyo@gmail.com>
2020-01-16 23:32:35 +0800

    Update

--- KaiserEngine/KeyboardEvent.cpp
+++ KaiserEngine/KeyboardEvent.cpp
@@ -1,8 +1,30 @@
 #include "stdafx.h"
 #include "KeyboardEvent.h"
 
+// @see: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getkeystate
+static
+inline
+bool
+IsKeyActive(int virtualKey)
+{
+    return (::GetKeyState(virtualKey) & 1) != 0;
+}
+
 KeyboardEvent::
 KeyboardEvent(UINT message, WPARAM wParam, LPARAM lParam)
+    : vk{ wParam & 0xFF }
+    , rc{ lParam & 0xFFFF }
+    , ek{ 0x1 & (lParam >> 24) }
+    , cc{ 0x1 & (lParam >> 29) }
+    , ks{ 0x1 & (lParam >> 30) }
+    , ts{ 0x1 & (lParam >> 31) }
+    , kslctrl{ ::IsKeyActive(VK_LCONTROL) }
+    , ksrctrl{ ::IsKeyActive(VK_RCONTROL) }
+    , kscapslock{ ::IsKeyActive(VK_CAPITAL) }
+    , kslshift{ ::IsKeyActive(VK_LSHIFT) }
+    , ksrshift{ ::IsKeyActive(VK_RSHIFT) }
+    , kslalt{ ::IsKeyActive(VK_LMENU) }
+    , ksralt{ ::IsKeyActive(VK_RMENU) }
 {
 }
 
--- KaiserEngine/KeyboardEvent.h
+++ KaiserEngine/KeyboardEvent.h
@@ -9,4 +9,47 @@ class KeyboardEvent
 public:
     KeyboardEvent(UINT message, WPARAM wParam, LPARAM lParam);
     virtual ~KeyboardEvent();
+private:
+    struct
+    {
+#pragma pack(push, 1)
+        // Virtual-key code
+        // @see: https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+        const int vk: 8;
+        // Repeat code
+        // The number of times the keystroke is autorepeated
+        // as a result of the user holding down the key.
+        // The repeat count is always 1 for a WM_KEYUP message
+        const int rc : 16;
+        // Extended key
+        // Indicates whether the key is an extended key, such as the right-hand ALT
+        // and CTRL keys that appear on an enhanced 101- or 102-key keyboard
+        // The value is 1 if it is an extended key; otherwise, it is 0
+        const int ek : 1;
+        // Context code
+        // The value is always 0 for a WM_KEYUP message
+        // The value is always 0 for a WM_KEYDOWN message
+        const int cc : 1;
+        // Previous key state
+        // The value is always 1 for a WM_KEYUP message
+        // The value is 1 if the key is down before the message is sent, or it is zero if the key is up
+        const int ks : 1;
+        // Transition state
+        // The value is always 1 for a WM_KEYUP message
+        // The value is always 0 for a WM_KEYDOWN message
+        const int ts : 1;
+#pragma pack(pop)
+        // KeyStates:
+        // <Control>
+        const bool kslctrl;     // Left
+        const bool ksrctrl;     // Right
+        // <CapsLock>
+        const bool kscapslock;
+        // <Shift>
+        const bool kslshift;    // Left
+        const bool ksrshift;    // Right
+        // <Alternative>
+        const bool kslalt;      // Left
+        const bool ksralt;      // Right
+    };
 };
