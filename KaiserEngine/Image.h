41e7bf1b34a4cd15f0950eed1b9ea6de8338c18b
KaiserKatze <donizyo@gmail.com>
2020-01-19 14:31:49 +0800

    Update Image.h

--- KaiserEngine/Image.h
+++ KaiserEngine/Image.h
@@ -3,25 +3,12 @@
 #include <string>
 #include <vector>
 
-struct PixelBGR
-{
-    unsigned char b;    // blue
-    unsigned char g;    // green
-    unsigned char r;    // red
-};
-
-struct PixelBGRA
-    : PixelBGR
-{
-    unsigned char a;    // alpha
-};
-
 class Image
 {
 protected:
     int width{ 0 };
     int height{ 0 };
-    std::vector<unsigned char> pixels;
+    std::vector<RGBQUAD> pixels;
 
 public:
     Image();
