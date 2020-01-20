d95fd1686f48e1e1a75f2ccfffd454f0304e6431
KaiserKatze <donizyo@gmail.com>
2020-01-20 19:37:30 +0800

    Update Image.h

--- KaiserEngine/Image.h
+++ KaiserEngine/Image.h
@@ -19,6 +19,8 @@ public:
     virtual int GetWidth() const;
     // The height of the image, in pixels
     virtual int GetHeight() const;
+    // The count of bits in a single pixel
+    virtual int GetBitCount() const;
     // The pixels, possibly in BGR or BGRA
     virtual const std::vector<RGBQUAD>& GetPixels() const;
 
