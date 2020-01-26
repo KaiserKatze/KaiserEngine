09112552b29503e9467d5968254e0df23711ac35
KaiserKatze <donizyo@gmail.com>
2020-01-26 20:48:07 +0800

    Update

---
+++ KaiserEngine/GLNamedObject.cpp
@@ -0,0 +1,19 @@
+#include "GLNamedObject.h"
+
+GLNamedObject::
+GLNamedObject()
+    : id{ 0 }
+{
+}
+
+GLNamedObject::
+~GLNamedObject()
+{
+}
+
+const GLuint&
+GLNamedObject::
+GetId() const
+{
+    return this->id;
+}
