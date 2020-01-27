4780762287ec7cba043a403a009be82ed629db69
KaiserKatze <donizyo@gmail.com>
2020-01-27 20:21:15 +0800

    Update

--- KaiserEngine/VertexData.h
+++ KaiserEngine/VertexData.h
@@ -11,25 +11,25 @@
 template <typename _Ty>
 struct VertexData
 {
-    struct      // positions
+    struct PositionArray
     {
         _Ty x;
         _Ty y;
         _Ty z;
         _Ty w;
-    };
-    struct      // colors
+    } positions;
+    struct ColorArray
     {
         _Ty r;
         _Ty g;
         _Ty b;
         _Ty a;
-    };
-    struct      // texture coordinates
+    } colors;
+    struct TextureArray
     {
         _Ty u;
         _Ty v;
-    };
+    } texture;
 
     using DataType = _Ty;
 
@@ -37,9 +37,9 @@ struct VertexData
     VertexData(const std::initializer_list<_Ty>& initializerList);
     ~VertexData() {}
 
-    const static int PositionElementCount = 4;      /* x, y, z, w */
-    const static int ColorElementCount = 4;         /* r, g, b, a */
-    const static int TextureElementCount = 2;       /* u, v */
+    const static int PositionElementCount   = sizeof(PositionArray) / sizeof(_Ty);  /* x, y, z, w */
+    const static int ColorElementCount      = sizeof(ColorArray)    / sizeof(_Ty);  /* r, g, b, a */
+    const static int TextureElementCount    = sizeof(TextureArray)  / sizeof(_Ty);  /* u, v */
 
     static void SetVertexAttribPointer();
     static void EnableVertexAttribArray();
@@ -49,18 +49,18 @@ struct VertexData
 template <typename _Ty>
 VertexData<_Ty>::
 VertexData()
-    : x{ 0 }, y{ 0 }, z{ 0 }, w{ 0 }
-    , r{ 0 }, g{ 0 }, b{ 0 }, a{ 0 }
-    , u{ 0 }, v{ 0 }
+    : positions{ 0, 0, 0, 0 }
+    , colors{ 0, 0, 0, 0 }
+    , texture{ 0, 0 }
 {
 }
 
-template<>
+template <>
 VertexData<float>::
 VertexData()
-    : x{ 0.0f }, y{ 0.0f }, z{ 0.0f }, w{ 0.0f }
-    , r{ 0.0f }, g{ 0.0f }, b{ 0.0f }, a{ 0.0f }
-    , u{ 0.0f }, v{ 0.0f }
+    : positions{ 0.0f, 0.0f, 0.0f, 0.0f }
+    , colors{ 0.0f, 0.0f, 0.0f, 0.0f }
+    , texture{ 0.0f, 0.0f }
 {
 }
 
@@ -69,7 +69,7 @@ VertexData<_Ty>::
 VertexData(const std::initializer_list<_Ty>& initializerList)
     : VertexData()
 {
-    _Ty* dataBegin = &(this->x);
+    _Ty* dataBegin = reinterpret_cast<_Ty*>(&(this->positions));
     std::copy(initializerList.begin(), initializerList.end(), dataBegin);
 }
 
@@ -81,32 +81,6 @@ SetVertexAttribPointer()
     throw std::runtime_error("Unsupported <typename _Ty>!");
 }
 
-static
-void
-SetVertexAttribPointer(
-    const GLuint& index,
-    const GLint& size,
-    const GLenum& type,
-    const GLboolean& normalized,
-    const GLsizei& stride,
-    const void* pointer)
-{
-    glVertexAttribPointer(index, size, type, normalized, stride, pointer);
-    {
-        std::stringstream ss;
-        ss << "glVertexAttribPointer("
-            << index << ", "
-            << size << ", "
-            << std::hex << type << std::dec << ", "
-            << std::boolalpha << normalized << std::noboolalpha << ", "
-            << stride << ", "
-            << pointer
-            << ")"
-            << std::endl;
-        DetectGLError(ss);
-    }
-}
-
 template <>
 void
 VertexData<float>::
@@ -114,7 +88,7 @@ SetVertexAttribPointer()
 {
     const GLenum type{ GL_FLOAT };
     const GLboolean normalized{ GL_FALSE };
-    const GLsizei stride{ sizeof(VertexData<float>) };
+    const GLsizei stride{ sizeof(VertexData) };
 
     const static int PositionByteCount = VertexData::PositionElementCount * sizeof(VertexData::DataType);
     const static int ColorByteCount = VertexData::ColorElementCount * sizeof(VertexData::DataType);
@@ -124,17 +98,41 @@ SetVertexAttribPointer()
     const static int ColorByteOffset = PositionByteOffset + PositionByteCount;
     const static int TextureByteOffset = ColorByteOffset + ColorByteCount;
 
+    static const auto setVertexAttribPointer0 = [](
+        const GLuint & index,
+        const GLint & size,
+        const GLenum & type,
+        const GLboolean & normalized,
+        const GLsizei & stride,
+        const void* pointer) -> void
+    {
+        glVertexAttribPointer(index, size, type, normalized, stride, pointer);
+        {
+            std::stringstream ss;
+            ss << "glVertexAttribPointer("
+                << index << ", "
+                << size << ", "
+                << std::hex << type << std::dec << ", "
+                << std::boolalpha << normalized << std::noboolalpha << ", "
+                << stride << ", "
+                << pointer
+                << ")"
+                << std::endl;
+            DetectGLError(ss);
+        }
+    };
+
 #pragma warning(push)
 #pragma warning(disable: 4312)
-    ::SetVertexAttribPointer(0,
+    setVertexAttribPointer0(0,
         VertexData<float>::PositionElementCount,
         type, normalized, stride,
         PositionByteOffset);
-    ::SetVertexAttribPointer(1,
+    setVertexAttribPointer0(1,
         VertexData<float>::ColorElementCount,
         type, normalized, stride,
         reinterpret_cast<void*>(ColorByteOffset));
-    ::SetVertexAttribPointer(2,
+    setVertexAttribPointer0(2,
         VertexData<float>::TextureElementCount,
         type, normalized, stride,
         reinterpret_cast<void*>(TextureByteOffset));
