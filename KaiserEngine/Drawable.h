fb8251316a6a83ccb7d8dcf674e211ecec9e113a
KaiserKatze <donizyo@gmail.com>
2020-01-29 10:59:32 +0800

    Update Drawable.h

--- KaiserEngine/Drawable.h
+++ KaiserEngine/Drawable.h
@@ -11,6 +11,9 @@ private:
     const DrawableContext* context;
 
 public:
+    Drawable();
+    ~Drawable();
+
     const std::vector<VertexData<float>>& GetVertexData() const;
     std::vector<VertexData<float>>& GetVertexData();
 };
